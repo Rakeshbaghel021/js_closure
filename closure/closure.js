d4f30aad6bb74b942a45edd91b2fa2eb51128e3d
Rakeshbaghel021 <rkrkumar021@gmail.com>
2019-09-08 22:56:57 +0530

    update js

--- closure/closure.js
+++ closure/closure.js
@@ -18,3 +18,40 @@ function nonsense(string){
 
 setTimeout(nonsense, 2000,"Hello")
 
+//3.
+function nonsense(string) {
+	var blab = function () {
+		return alert(string);
+	}
+	return blab;
+}
+
+//4.
+
+function nonsense(string) {
+	var blab = function () {
+		return alert(string);
+	}
+	return blab;
+}
+var blabLater = nonsense("Hello");
+var blabAgainLater = nonsense("Hello Again")
+
+//5.
+
+var lastNameTrier = function(firstName) {
+    var innerFunction = function(lastName) {
+				console.log(firstName + " " + lastName)
+    };
+	
+		return innerFunction;
+};
+var firstNameFarmer = lastNameTrier('Farmer'); //logs nothing
+firstNameFarmer('Brown'); //logs 'Farmer Brown'
+
+firstNameFarmer('Jane'); //logs 'Farmer Jane'
+firstNameFarmer('Lynne'); //logs 'Farmer Lynne'
+
+
+//6.
+
